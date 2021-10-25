57c96dd97f6f3decdba32337e46709e1bbded2d4
= <=>
2021-10-25 18:39:10 -0400

    final revisions

--- plugins/router-actions.client.js
+++ plugins/router-actions.client.js
@@ -1,12 +1,17 @@
 export default ({ app: { router, store } }) => {
+    router.beforeEach((to, from, next) => {
+        window.scrollTo(0, 0);
+        next();
+    })
     router.afterEach((to, from) => {
         // console.log(to.path, from.path)
-        window.scrollTo(0, 0);
+
 
         if (from.path == '/2') {
             if (store.state.occupation.isFlyoverCompleted) {
                 store.commit(`occupation/setFirstVisit`);
             }
         }
+
     })
   }
